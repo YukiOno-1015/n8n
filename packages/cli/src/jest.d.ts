5f98f14c0f718c50e7445b0a6d1c155fe8ff7c78
jqit-yukiono <jqit.yuki.ono@gmail.com>
2026-04-20 11:19:10 +0900

    Jest legacy matcherの型互換を追加

--- packages/cli/src/jest.d.ts
+++ packages/cli/src/jest.d.ts
@@ -1,7 +1,11 @@
 namespace jest {
 	interface Matchers<R, T> {
+		toBeCalled(): T;
+		toBeCalledTimes(expected: number): T;
+		toBeCalledWith(...args: unknown[]): T;
 		toBeEmptyArray(): T;
 		toBeEmptySet(): T;
 		toBeSetContaining(...items: string[]): T;
+		toThrowError(expected?: unknown): T;
 	}
 }
